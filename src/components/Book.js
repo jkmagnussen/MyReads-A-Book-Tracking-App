d87461f9b3e0b2cc36fead838982c660ec37a477
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-02-24 14:40:26 +0000

    Amended copy

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import * as BooksAPI from "../BooksAPI";
 
 const Book = props => {
--- src/components/Search.js
+++ src/components/Search.js
@@ -19,8 +19,12 @@ const Search = props => {
         console.log(books);
         if (!books.error) {
           setSearchedBooks(books);
+        } else {
+          setSearchedBooks([]);
         }
       });
+    } else {
+      setSearchedBooks([]);
     }
   };
 
@@ -32,14 +36,6 @@ const Search = props => {
         </button>
         <div className="search-books-input-wrapper">
           <form onSubmit={event => handleSubmit(event)}>
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
             <input
               type="text"
               placeholder="Search by title or author"
